dcdd1e0f689f2cdb817320a206c8b8c4136c12c0
dmitridzuk <dmitridzuk3@gmail.com>
2024-01-29 13:42:47 +0200

    Update main.cpp

--- laba2/main.cpp
+++ laba2/main.cpp
@@ -3,12 +3,26 @@
 int main()
 {
     float dlina, shirina;
+    
     std::cout << "input dlina rectangle: ";
-    std::cin  >> dlina ;
+    std::cin >> dlina;
+    
     std::cout << "input shirina rectangle: ";
     std::cin >> shirina;
-    Rectangle rectangle(dlina, shirina);
-    std::cout << "Ploshad: " << rectangle.S() << "\n";
-    std::cout << "Perimetr: " << rectangle.P() << "\n";
-    std::cout << "Diagonal: " << rectangle.D() << "\n";
+    
+    if ((dlina > 0) && (shirina > 0)) {
+            
+        Rectangle rectangle(dlina, shirina);
+            
+        std::cout << "Ploshad: " << rectangle.S() << "\n";
+        std::cout << "Perimetr: " << rectangle.P() << "\n";
+        std::cout << "Diagonal: " << rectangle.D() << "\n";
+        
+    }
+    
+    else
+    {
+        std::cout << "incorrect rectangle";
+        return 0;
+    }
 }
