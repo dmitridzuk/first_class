6cbf79ddf47eb857e1459f5919ad7c9f87cdf799
dmitridzuk <dmitridzuk3@gmail.com>
2024-03-29 22:27:44 +0200

    .
    .

--- c++/laba6/main.cpp
+++ c++/laba6/main.cpp
@@ -2,32 +2,27 @@
 
 int main()
 {
-    // Создание окна
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML Image");
-
-    // Загрузка изображений
     sf::Texture backgroundTexture;
     if (!backgroundTexture.loadFromFile("1.jpg"))
     {
-        return -1; // Проверка на успешную загрузку изображения
+        return -1; 
     }
     sf::Sprite background(backgroundTexture);
 
     sf::Texture spaceshipTexture;
     if (!spaceshipTexture.loadFromFile("2.jpg"))
     {
-        return -1; // Проверка на успешную загрузку изображения
+        return -1; 
     }
     sf::Sprite spaceship(spaceshipTexture);
 
-    // Начальные позиции изображений
     background.setPosition(0, 0);
     spaceship.setPosition(400, 300);
 
-    // Главный цикл приложения
     while (window.isOpen())
     {
-        // Обработка событий
+
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -36,8 +31,6 @@ int main()
                 window.close();
             }
         }
-
-        // Управление перемещением корабля с помощью клавиш
         sf::Vector2f movement(0.f, 0.f);
         float speed = 1/4.f;
 
@@ -51,13 +44,10 @@ int main()
             movement.x += speed;
 
         spaceship.move(movement);
-
-        // Отрисовка изображений
         window.clear();
         window.draw(background);
         window.draw(spaceship);
         window.display();
     }
-
     return 0;
 }
