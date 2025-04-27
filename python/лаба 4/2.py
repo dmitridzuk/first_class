d4b8d7f6f652df99bed1a41ed476ac96729f9828
dmitridzuk <144421509+dmitridzuk@users.noreply.github.com>
2025-04-27 20:09:47 +0200

    Delete python/лаба 4 directory

--- python/лаба 4/2.py
+++
@@ -1,6 +0,0 @@
-def chtoto(s1, s2):
-    return s1.issubset(s2)
-s1 = set(input("Введите элементы первого множества через запятую: ").split(","))
-s2 = set(input("Введите элементы второго множества через запятую: ").split(","))
-a = chtoto(s1, s2)
-print(a)
