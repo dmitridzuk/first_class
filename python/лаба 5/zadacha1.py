9dcc0ea57c7ccb0e13bb302513c8459c000067c1
dmitridzuk <144421509+dmitridzuk@users.noreply.github.com>
2025-04-27 20:10:34 +0200

    Delete python/лаба 5 directory

--- python/лаба 5/zadacha1.py
+++
@@ -1,9 +0,0 @@
-with open('C:\\Users\\student24\\Desktop\\input.txt', 'r') as file:
-    numbers = list(map(int, file.readline().split()))
-
-pro = 1
-for i in numbers:
-    pro *= i
-
-with open('C:\\Users\\student24\\Desktop\\output.txt', 'w') as file:
-    file.write(str(pro))
--- python/лаба 5/zadacha2.py
+++
@@ -1,7 +0,0 @@
-with open('C:\\Users\\student24\\Desktop\\input.txt', 'r') as file:
-    f = [int(line.strip()) for line in file.readlines()]
-f.sort()
-
-with open('C:\\Users\\student24\\Desktop\\output.txt', 'w') as file:
-    for i in f:
-        file.write(str(i) + '\n')
